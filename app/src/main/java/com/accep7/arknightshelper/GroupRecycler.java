67cda69f7c4f5b903afef54523016c738ebdb6f1
Accep7 <omichwtf@gmail.com>
2022-01-17 21:53:58 +0300

    Implemented combinatorics group filtering algorithm

--- app/src/main/java/com/accep7/arknightshelper/GroupRecycler.java
+++ app/src/main/java/com/accep7/arknightshelper/GroupRecycler.java
@@ -3,26 +3,43 @@ package com.accep7.arknightshelper;
 import static com.accep7.arknightshelper.RecruitmentPool.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class GroupRecycler {
+
+    //Represents the max amount of tags user can select in the game and create combinations from
+    final static int MAX_TAG_COMBO = 3;
+
+    static List<String> selectedTags;
+
     public static List<ItemRecycler> getEntries(List<String> tags) {
+        //selectedTags.clear();
+        selectedTags = tags;
+        int sample = MAX_TAG_COMBO;
         List<ItemRecycler> result = new ArrayList<>();
-        for (RecruitableOperator operator : recruitableOperators) {
-            if (operator.isExpandedDetails()) {
-                operator.setExpandedDetails(false);
-            }
-            if (!tags.contains(QUALIFICATION_TOP) && operator.qualification != null &&
-                    operator.qualification.equals(QUALIFICATION_TOP)) {
-                continue;
-            }
-            List<String> intersectedTags = intersection(tags, operator.getTags());
-            if (!intersectedTags.isEmpty()) {
-                addOperator(result, intersectedTags, operator);
-            }
+
+        for (int i = 0; i < MAX_TAG_COMBO; i++) {
+            List<String[]> combinations = generateTagCombinations(selectedTags.size(), sample);
+            for (String[] combination : combinations) {
+                List<String> combo = Arrays.asList(combination);
+                for (RecruitableOperator operator : recruitableOperators) {
+                    if (operator.isExpandedDetails()) {
+                        operator.setExpandedDetails(false);
+                    }
+                    if (!combo.contains(QUALIFICATION_TOP) && operator.qualification != null &&
+                            operator.qualification.equals(QUALIFICATION_TOP)) {
+                        continue;
+                    }
+                    List<String> intersectedTags = intersection(combo, operator.getTags());
+                    if (!intersectedTags.isEmpty()) {
+                        addOperator(result, intersectedTags, operator);
+                    }
+                }
+                Collections.sort(result);            }
+            sample--;
         }
-        Collections.sort(result);
         return result;
     }
 
@@ -50,14 +67,26 @@ public class GroupRecycler {
                 itemRecycler.addOperator(operator);
                 added = true;
             }
-            if (tags.size() > 1) {
-                for (String tag : tags) {
-                    addOperator(resultList, Collections.singletonList(tag), operator);
-                }
-            }
         }
         if (!added) {
             resultList.add(new ItemRecycler(tags, operator));
         }
+    }
+
+    private static void generator(List<String[]> combinations, String[] data, int start, int end, int index) {
+        if (index == data.length) {
+            String[] combination = data.clone();
+            combinations.add(combination);
+        } else if (start <= end) {
+            data[index] = selectedTags.get(start);
+            generator(combinations, data, start + 1, end, index + 1);
+            generator(combinations, data, start + 1, end, index);
+        }
+    }
+
+    private static List<String[]> generateTagCombinations(int n, int r) {
+        List<String[]> combinations = new ArrayList<>();
+        generator(combinations, new String[r], 0, n - 1, 0);
+        return combinations;
     }
 }
